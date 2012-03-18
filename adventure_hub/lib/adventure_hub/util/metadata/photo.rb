82d09657e8019e14b6ca9819df9114e414c7d428
Scott Fleckenstein <nullstyle@gmail.com>
2012-03-18 16:30:29 -0700

    make photo extractor return a symbol hash

--- adventure_hub/lib/adventure_hub/util/metadata/photo.rb
+++ adventure_hub/lib/adventure_hub/util/metadata/photo.rb
@@ -6,10 +6,14 @@ module AdventureHub
         include Celluloid
 
         def extract(path)
-          json    = Util::ShellRunner.new("exiftool -json #{path}").stdout
-          data    = MultiJson.decode(json)
-          
-          exif = data.first
+          json  = Util::ShellRunner.new("exiftool -json #{path}").stdout
+          data  = MultiJson.decode(json)
+          exif  = data.first
+
+          {
+            exif: exif,
+            occurred_at: Time.parse(exif["CreateDate"]),
+          }
         end
       end
 
