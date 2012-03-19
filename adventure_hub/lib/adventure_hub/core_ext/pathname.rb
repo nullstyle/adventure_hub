5c342b3588e4afc59466592f07c3a3b1cec918c7
Scott Fleckenstein <nullstyle@gmail.com>
2012-03-18 22:53:54 -0700

    Add Pathname#parent_of?, rename Pathname#underneath? to Pathname#child_of?

--- adventure_hub/lib/adventure_hub/core_ext/pathname.rb
+++ adventure_hub/lib/adventure_hub/core_ext/pathname.rb
@@ -1,7 +1,11 @@
 class Pathname
-  def underneath?(other)
+  def child_of?(other)
     return false if cleanpath == other.cleanpath
 
     !!(cleanpath.to_s =~ Regexp.new("^#{other.cleanpath.to_s}"))
+  end
+
+  def parent_of?(other)
+    other.child_of?(self)
   end
 end
